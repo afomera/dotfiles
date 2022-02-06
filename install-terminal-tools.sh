4917ee7a1d9eec41260abd6d65e0349f73d6c1d9
Andrea Fomera <afomera@hey.com>
2022-02-05 17:17:23 -0700

    try and wrap install terminal tools to be a bit safer on extra runs

--- install-terminal-tools.sh
+++ install-terminal-tools.sh
@@ -19,24 +19,32 @@ if ! command -v brew > /dev/null; then
   export PATH="/usr/local/bin:$PATH"
 fi
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Check for Oh My Zsh and install if we don't have it
+# also installs zsh-autosuggestions when we first install oh-my-zsh
+if ! command -v omz > /dev/null; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  # Install zsh-autosuggestions plugin
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+if test ! $(which asdf); then
+  # Install asdf
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+  # Re-source asdf into the current shell
+  source ~/.asdf/asdf.sh
+
+  # Install asdf-ruby
+  asdf plugin add ruby
+
+  # Install asdf-nodejs
+  asdf plugin-add nodejs
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+fi
 
 # Re-source asdf into the current shell
 source ~/.asdf/asdf.sh
-
-# Install asdf-ruby
-asdf plugin add ruby
-
-# Install asdf-nodejs
-asdf plugin-add nodejs
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
 # Install local dependencies
 asdf install
