497b96045c3d0b1db3d283de9ee4d9f2ba5441af
Andrea Fomera <afomera@hey.com>
2026-02-25 15:49:13 -0700

    git please for push with lease

--- home/private_dot_config/git/aliases.zsh
+++ home/private_dot_config/git/aliases.zsh
@@ -6,4 +6,6 @@ alias gf="git fetch"
 alias ga="git add"
 alias ga.="git add ."
 alias gc="git commit"
+alias gp="git push"
+alias gpl="git pull"
 alias gitclean='git branch --merged | egrep -v "(^\*|master|develop|main)" | xargs git branch -d; git remote prune origin'
