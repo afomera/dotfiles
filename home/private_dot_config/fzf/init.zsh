58811888759b827ac089e41d582159e7a98d04f6
Andrea Fomera <afomera@hey.com>
2026-02-25 13:29:43 -0700

    Add themes for git diff, lsd, bat

--- home/private_dot_config/fzf/init.zsh
+++ home/private_dot_config/fzf/init.zsh
@@ -2,7 +2,12 @@
 if command -v fzf &>/dev/null; then
   source <(fzf --zsh)
 
-  export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
+  export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border \
+    --color=fg:#e6edf3,bg:#0d1117,hl:#79c0ff \
+    --color=fg+:#e6edf3,bg+:#161b22,hl+:#79c0ff \
+    --color=info:#8b949e,prompt:#7ee787,pointer:#d2a8ff \
+    --color=marker:#ffa657,spinner:#d2a8ff,header:#8b949e \
+    --color=border:#30363d,gutter:#0d1117"
 
   # Use ripgrep for file search if available
   if command -v rg &>/dev/null; then
