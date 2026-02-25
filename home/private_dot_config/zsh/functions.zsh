385cf83ef6d3afdc6552b7e94f60cab41b76246e
Andrea Fomera <afomera@hey.com>
2026-02-25 15:47:27 -0700

    psg, port, ip, flush_dns, b64/b64d functions

--- home/private_dot_config/zsh/functions.zsh
+++ home/private_dot_config/zsh/functions.zsh
@@ -1,5 +1,21 @@
 #!/usr/bin/env zsh
 
+function psg() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: psg <pattern>"
+    return 1
+  fi
+  ps aux | grep -i "$1" | grep -v "grep"
+}
+
+function port() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: port <port>"
+    return 1
+  fi
+  lsof -i:"$1" -P -n | head -20
+}
+
 function kill_port() {
   if [[ -z "$1" ]]; then
     echo "Usage: kill_port <port>"
@@ -12,6 +28,33 @@ function kill_port() {
   fi
   echo "$pids" | xargs kill -9
   echo "Killed process(es) on port $1"
+}
+
+function ip() {
+  echo "Local:  $(ipconfig getifaddr en0 2>/dev/null || echo 'not connected')"
+  echo "Public: $(curl -4 -s ifconfig.me)"
+}
+
+function flush_dns() {
+  sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder
+  echo "DNS cache flushed"
+}
+
+function b64() {
+  if [[ -z "$1" ]]; then
+    base64
+  else
+    echo -n "$1" | base64
+  fi
+}
+
+function b64d() {
+  if [[ -z "$1" ]]; then
+    base64 --decode
+  else
+    echo -n "$1" | base64 --decode
+    echo
+  fi
 }
 
 function gitwork() {
