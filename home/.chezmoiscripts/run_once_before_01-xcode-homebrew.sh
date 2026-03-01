fd57db2ee79218d7b3cec1cf84613b6049ce2018
Andrea Fomera <afomera@hey.com>
2026-02-28 20:35:04 -0500

    fixes

--- home/.chezmoiscripts/run_once_before_01-xcode-homebrew.sh
+++ home/.chezmoiscripts/run_once_before_01-xcode-homebrew.sh
@@ -12,6 +12,6 @@ fi
 # Homebrew
 if ! command -v brew >/dev/null 2>&1; then
   echo "==> Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
